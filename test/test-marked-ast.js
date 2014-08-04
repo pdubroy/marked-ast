6b343b5f9cab04b452ba44466761fed5276a8e13
Patrick Dubroy <pdubroy@gmail.com>
2014-08-04 14:21:39 -0700

    Add a test which runs the original marked tests with the modified version.

--- test/test-marked-ast.js
+++ test/test-marked-ast.js
@@ -1,9 +1,32 @@
-var test = require('tape');
+var fork = require('child_process').fork,
+    test = require('tape'),
+    util = require('util');
 
 var marked = require('marked'),
     markedMod = require('../');
 
-require('./chjj-marked/test/').runTests();
+function runProcess(relativePath, cb) {
+  var path = __dirname + '/' + relativePath;
+  var child = fork(path, { silent: true });
+  child.on('exit', function(code) {
+    var out, err;
+    child.stdout.on('data', function(chunk) { out += chunk; });
+    child.stderr.on('data', function(chunk) { err += chunk; });
+    cb(code, out, err);
+  });
+}
+
+test('chjj-marked tests', function(t) {
+  // Compare the results of the original marked tests with the modified tests.
+  runProcess('../third_party/marked/test/', function(code, expectedOut, err) {
+    t.equal(code, 0);
+    runProcess('/chjj-marked/test/', function(code, out, err) {
+      t.equal(code, 0);
+      t.equal(out, expectedOut);
+      t.end();
+    });
+  });
+});
 
 test('rewrite to string', function (t) {
   var text = '_This is **Markdown**_, he said.';
